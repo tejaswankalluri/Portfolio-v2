5a7196b4d88784040a95e2a63ead84dce78c5dd6
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-11-15 10:52:28 +0530

    Added: SEO

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Tejaswan - My Portfolio";
 export const SITE_DESCRIPTION =
     "This website is about me. Hi this is Tejaswan, I am a web developer";
+export const SITE_URL = "https://tejaswan.me";
