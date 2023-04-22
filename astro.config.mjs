5823da61885dc10df1105700cf4f064f9a716f02
Tejaswan Kalluri <tejaswan@turnkey.work>
2023-04-22 22:24:23 +0530

    Update: Optimised images with .webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://tejaswan.me/",
   integrations: [
     sitemap(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
 });
